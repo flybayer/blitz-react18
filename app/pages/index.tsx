862aae79c794462b5b6163c2d3b23e433a8e29dd
Brandon Bayer <b@bayer.ws>
2021-07-07 18:17:31 -0400

    update

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,14 @@
-import { BlitzPage } from "blitz"
+import { BlitzPage, Link, Router } from "blitz"
 
 const Home: BlitzPage = () => {
-  return <h1>Dashboard page</h1>
+  return (
+    <div>
+      <h1>Dashboard page</h1>
+      <a href="/protected">
+        Click to go to /protected and be redirected back here with corrupted page
+      </a>
+    </div>
+  )
 }
 
 export default Home
